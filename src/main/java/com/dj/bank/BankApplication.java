4f226445055ca9af322677e049b959fb3af08cb3
hmm <hmm17713260527@163.com>
2020-03-05 18:51:19 +0800

    启动类助解

--- src/main/java/com/dj/bank/BankApplication.java
+++ src/main/java/com/dj/bank/BankApplication.java
@@ -1,9 +1,13 @@
 package com.dj.bank;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@MapperScan("com.dj.bank.mapper")
+@EnableTransactionManagement
 public class BankApplication {
 
     public static void main(String[] args) {
