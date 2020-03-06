cd34215c41a8c4e7a046c4a2ac2a53ec7186354c
lwf <lwf930811@163.com>
2020-03-06 21:00:14 +0800

    申请银行卡

---
+++ src/main/java/com/dj/bank/util/Random.java
@@ -0,0 +1,9 @@
+package com.dj.bank.util;
+
+public class Random {
+	 
+	    //����õ�4λ��
+	    public static int getRandNum() {
+	        return (int)((Math.random()*9+1)*1000);
+	    }
+}
---
+++ src/main/java/com/dj/bank/web/page/BankCardPageController.java
@@ -0,0 +1,39 @@
+package com.dj.bank.web.page;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.dj.bank.common.SystemConstant;
+import com.dj.bank.pojo.BaseData;
+import com.dj.bank.service.BaseDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+/**
+ * @ProjectName: bank
+ * @Package: com.dj.bank.web.page
+ * @ClassName: BankCardPageController
+ * @Author: Liuwf
+ * @Description: 银行卡
+ * @Date: 2020/3/6 20:17
+ * @Version: 1.0
+ */
+@Controller
+@RequestMapping("/bankCard/")
+public class BankCardPageController {
+    @Autowired
+    private BaseDataService baseDataService;
+
+    @RequestMapping("toAdd")
+    private String toAdd(Model model) {
+        QueryWrapper<BaseData> baseDataQueryWrapper = new QueryWrapper<>();
+        baseDataQueryWrapper.eq("p_id", SystemConstant.BANK_TYPE_PID);
+        List<BaseData> baseDataList = baseDataService.list(baseDataQueryWrapper);
+        model.addAttribute("baseDataList",baseDataList);
+        return "bank_card/bank_card_add";
+    }
+
+
+}
