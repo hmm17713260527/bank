a3e0b667420e13443e1ca9f3fe8be04f3fb0e4ba
wbb <w1016798002@163.com>
2020-03-07 16:21:39 +0800

    展示普通积分总和

---
+++ src/main/java/com/dj/bank/service/BankProductService.java
@@ -0,0 +1,10 @@
+package com.dj.bank.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.dj.bank.pojo.BankProduct;
+
+/**
+ * @author 86150
+ */
+public interface BankProductService extends IService<BankProduct> {
+}
